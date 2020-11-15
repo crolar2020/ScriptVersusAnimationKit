27487b429e3a7d126d7fbb71963c1d3cf840c8f5
Larry Crochet <crolar2015@gmail.com>
2020-11-14 23:31:03 -0500

    anim plane

---
+++ Assets/Plugins/uploadKit.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\crola\Documents\unity3d\unityprojects\ScriptVersusAnimationKit\1604063145435857894_wbr2_kit_assignment.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1604063145435857894.json
