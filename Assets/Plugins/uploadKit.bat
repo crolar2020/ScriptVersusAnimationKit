2eaf5c1a8f6fca4bebbffe4c37a72e380b85aede
Larry Crochet <crolar2015@gmail.com>
2020-11-19 21:18:23 -0500

    add arrow

--- Assets/Plugins/uploadKit.bat
+++ Assets/Plugins/uploadKit.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\crola\Documents\unity3d\unityprojects\ScriptVersusAnimationKit\1606597486367998565_wbr2_kit_assignment.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1606597486367998565.json
+curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\crola\Documents\unity3d\unityprojects\ScriptVersusAnimationKit\1606597486367998565_scriptvsanimprefabkit.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1606597486367998565.json
